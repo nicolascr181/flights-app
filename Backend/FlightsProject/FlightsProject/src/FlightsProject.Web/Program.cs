38afb18dc7424dbdb68e7f05ddfe1da65f8f35ef
Nicolas Cardona <ncardona@sigmaingenieria.net>
2024-04-27 15:17:04 -0500

    chore: Add template backend
    Template backend for clean arquitecture

---
+++ Backend/FlightsProject/FlightsProject/src/FlightsProject.Web/Program.cs
@@ -0,0 +1,29 @@
+﻿
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+
+if (builder.Environment.IsDevelopment())
+{
+
+}
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+  app.UseDeveloperExceptionPage();
+}
+
+app.MapControllers();
+
+app.UseHttpsRedirection();
+
+app.Run();
+
+
+// Make the implicit Program.cs class public, so integration tests can reference the correct assembly for host building
+public partial class Program
+{
+}
