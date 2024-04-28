7f6ecb757f754ea29a4ba99a24db1c7e277dab17
Nicolas Cardona <ncardona@sigmaingenieria.net>
2024-04-28 16:14:36 -0500

    chore: prepare application layer

---
+++ Backend/FlightsProject/FlightsProject/src/FlightsProject.UseCases/Data/IApplicationDbContext.cs
@@ -0,0 +1,10 @@
+﻿using FlightsProject.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace FlightsProject.UseCases.Data;
+public interface IApplicationDbContext
+{
+  DbSet<Journey> Journeys { get; set; }
+
+  Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+}
---
+++ Backend/FlightsProject/FlightsProject/src/FlightsProject.UseCases/DependencyInjection.cs
@@ -0,0 +1,22 @@
+﻿using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace FlightsProject.UseCases;
+public static class DependencyInjection
+{
+  public static IServiceCollection AddApplication(this IServiceCollection services)
+  {
+    services.AddMediatR(config => {
+      config.RegisterServicesFromAssemblyContaining<ApplicationAssemblyReference>();
+    });
+
+    services.AddScoped(
+        typeof(IPipelineBehavior<,>)
+        //typeof(ValidationBehavior<,>)
+    );
+
+    //services.AddValidatorsFromAssemblyContaining<ApplicationAssemblyReference>();
+
+    return services;
+  }
+}
