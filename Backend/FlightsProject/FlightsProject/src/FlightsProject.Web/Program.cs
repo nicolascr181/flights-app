ad78da42e6280f8610b9fb0cf686ed5d8798f13c
Nicolas Cardona <ncardona@sigmaingenieria.net>
2024-04-28 13:01:11 -0500

    chore: support for swagger

--- Backend/FlightsProject/FlightsProject/src/FlightsProject.Web/Program.cs
+++ Backend/FlightsProject/FlightsProject/src/FlightsProject.Web/Program.cs
@@ -3,6 +3,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 if (builder.Environment.IsDevelopment())
 {
@@ -14,6 +16,8 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
   app.UseDeveloperExceptionPage();
+  app.UseSwagger();
+  app.UseSwaggerUI();
 }
 
 app.MapControllers();
