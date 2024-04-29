53f7e3b4f65492626589a77a2b563fb410429b2b
Nicolas Cardona <ncardona@sigmaingenieria.net>
2024-04-28 20:18:31 -0500

    chore: configure dependency injections for infraestructure layer

--- Backend/FlightsProject/FlightsProject/src/FlightsProject.Infrastructure/DependencyInjection.cs
+++ Backend/FlightsProject/FlightsProject/src/FlightsProject.Infrastructure/DependencyInjection.cs
@@ -1,11 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using FlightsProject.Core.Interfaces;
+using FlightsProject.Core.Primitives;
+using FlightsProject.Infrastructure.Persistence;
+using FlightsProject.Infrastructure.Persistence.Repositories;
+using FlightsProject.UseCases.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace FlightsProject.Infrastructure;
 
-  internal class DependencyInjection
+public static class DependencyInjection
   {
+  public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+  {
+    services.AddPersistence(configuration);
+    return services;
   }
+
+  private static IServiceCollection AddPersistence(this IServiceCollection services, IConfiguration configuration)
+  {
+
+    services.AddDbContext<ApplicationDbContext>(options => options.UseSqlite(configuration.GetConnectionString("SqliteConnection")));
+
+    services.AddScoped<IApplicationDbContext>(sp =>
+            sp.GetRequiredService<ApplicationDbContext>());
+
+    services.AddScoped<IUnitOfWork>(sp =>
+            sp.GetRequiredService<ApplicationDbContext>());
+
+    services.AddScoped<IJourneyRepository, JourneyRepository>();
+
+    return services;
+  }
+}
