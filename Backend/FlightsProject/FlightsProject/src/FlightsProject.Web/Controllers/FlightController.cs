1de9a1c93ed51f25508701e9b705843c29ec39eb
Nicolas Cardona <ncardona@sigmaingenieria.net>
2024-04-29 01:11:09 -0500

    chore: add flight controller

---
+++ Backend/FlightsProject/FlightsProject/src/FlightsProject.Web/Controllers/FlightController.cs
@@ -0,0 +1,32 @@
+﻿using FlightsProject.UseCases.Flights.List;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FlightsProject.Web.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class FlightController : APIController
+{
+  private readonly ISender _mediator;
+
+  public FlightController(ISender mediator)
+  {
+    _mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
+  }
+
+  /// <summary>
+  /// List all flights without filters
+  /// </summary>
+  /// <returns></returns>
+  [HttpGet]
+  public async Task<IActionResult> ListFlights()
+  {
+    var flightsResult = await _mediator.Send(new ListFlightsQuery());
+
+    return flightsResult.Match(
+        journey => Ok(journey),
+        errors => Problem(errors)
+    );
+  }
+}
